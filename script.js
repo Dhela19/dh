d19263bd86a8a71198bc86f07aeda41e454d1e5f
Ubongabasi Jerome <87214555+Dhela19@users.noreply.github.com>
2023-01-26 14:09:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function warning(){let wID=setTimeout(function set(){document.getElementById("info").innerHTML="This Version is Depreciated, Tap the button below t
-o move to the newest version";},4000);}
+function warning(){let wID=setTimeout(function set(){document.getElementById("info").innerHTML="This Version is Depreciated, Tap the button below to move to the newest version";
+},4000);}
 warning()
  function redirect(){ let tID=setTimeout(function deed(){
 alert("Redirecting to Version 2.1 in 7 seconds");
