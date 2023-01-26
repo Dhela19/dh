b21253fdc003aa269b0f1d0c8c8bb5d665bd2c92
Ubongabasi Jerome <87214555+Dhela19@users.noreply.github.com>
2023-01-26 13:45:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-document.getElementById("terminal").innerHTML="Jan 15 Update";
-     function redirect(){
-      document.getElementById("terminal").innerHTML="This Version is Depreciated, Redirecting to the newest version");
-alert("Redirecting to 2.1"
+ function redirect(){
+      document.getElementById("info").innerHTML="This Version is Depreciated, Redirecting to the newest version");
+alert("Redirecting to 2.1")
 window.location.href="https://dh2-0.vercel.app"
 }
 //the temperature value is used to set the randomness of the output 
